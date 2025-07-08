c4d48579028c84fb15abbad738b9b8a59da3501c
Anisha493 <karkianisha84@gmail.com>
2025-07-08 12:35:31 +0545

    Update productControllers.js

--- E-commerce/src/controllers/productControllers.js
+++ E-commerce/src/controllers/productControllers.js
@@ -1,7 +1,47 @@
+import Product from "../models/Product.js"
 
 
-const sayHello = (req,res)=>{
-    res.send('I am from router page')
+const createProduct = async (req,res)=>{
+    const {ram,rom,productName,description,price,gen, brand}=req.body
+    try{
+        //console.log(ram,rom,productName,description,price)
+
+    if(!ram) {throw new Error('ram is required')}
+    if(!rom) {throw new Error("rom is required")}
+    if(!productName || !description || !price || !gen || !brand) {
+        throw new Error("Credential missing")
+     }
+    
+    const data = await Product.create({
+        productName:productName,
+        ram:ram,
+        rom:rom,
+        price:price,
+        description:description,
+        gen:gen,
+        brand:brand
+    })
+    res.send(data)
+    } catch (error){
+        console.log(error.message)
+        res.status(400).send(error.message)
+    }
+    
 }
 
-export {sayHello}
+const getAllProduct = async (req,res)=>{
+    try{
+        const data = await Product.find()
+        res.status(200).json({data})
+    } catch{
+        console.log(error.message)
+        res.status(400).send(error.message)
+    }
+}
+const getAllProductById =async (req,res) => {
+  const id = req.params.id;
+  const data = await Product.findById(id);
+  res.status(200).json({message:"get my singleProduct", data});
+}
+
+export {createProduct, getAllProduct, getAllProductById};

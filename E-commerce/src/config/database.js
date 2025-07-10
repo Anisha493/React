8f7a38dbff9588521e6103f6b9be19bb63490fbb
Anisha493 <karkianisha84@gmail.com>
2025-07-10 14:08:02 +0545

    Update database.js

--- E-commerce/src/config/database.js
+++ E-commerce/src/config/database.js
@@ -1,13 +1,29 @@
 import mongoose from "mongoose";
+import User from "../models/User.js";
 
 const connectDb = async()=>{
     try{
     await mongoose.connect('mongodb+srv://user:user@cluster0.n2m4uuh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
     console.log("database connected successfully")
-} catch (error){
-    console.log("error.message")
-}
-    
-}
 
-export default connectDb;
+        const adminExist = await User.findOne({email: "admin@gmail.com"})
+   
+
+    if(adminExist){
+        console.log("Admin already exists")
+        return
+    }else{
+        await User.create({
+            userName: "AdminUser",
+            email: "admin@gmail.com",
+            password: "admin123",
+            role: "ADMIN"
+        })
+        console.log("Admin seeded successfully")
+    }
+
+    } catch (error){
+        console.log(error.message)
+    }
+}
+export default connectDb;

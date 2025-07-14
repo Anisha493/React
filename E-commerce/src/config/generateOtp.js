f42c85b10c19698898d090236b7051d695f09015
Anisha493 <karkianisha84@gmail.com>
2025-07-14 12:35:33 +0545

    Update generateOtp.js

--- E-commerce/src/config/generateOtp.js
+++ E-commerce/src/config/generateOtp.js
@@ -1,1 +1,11 @@
+import otpGenerator from "otp-generator";
 
+const generateOtp = () => {
+const otp = otpGenerator.generate(6, {
+            upperCaseAlphabets: false,
+            specialChars: false,
+            lowerCaseAlphabets: false,
+        });
+        return otp;
+    }
+    export default generateOtp;

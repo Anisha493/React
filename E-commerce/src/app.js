4c52d651c79562ca3013293839f203a1bd63ee0f
Anisha493 <karkianisha84@gmail.com>
2025-07-18 12:00:20 +0545

    Update app.js

--- E-commerce/src/app.js
+++ E-commerce/src/app.js
@@ -2,6 +2,9 @@ import express from 'express';
 import productRoutes from './routes/productRoutes.js'
 import authRoute from './routes/authRoute.js'
 import connectDb from './config/database.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express()
 app.use(express.json())
